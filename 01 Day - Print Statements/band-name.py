bb2f7f49fff33e86d0a57cd3792929a9abb0426b
Wasim Hyder <wasimhyder09@gmail.com>
2026-02-13 12:52:53 +0500

    Day 1 - Print statemetns

---
+++ 01 Day - Print Statements/band-name.py
@@ -0,0 +1,4 @@
+city_name = input("What is name of your city?\n")
+pet_name = input("What is name of your pet?\n")
+
+print("Name of your band is " + city_name + " " + pet_name)
---
+++ 01 Day - Print Statements/print.py
@@ -0,0 +1,10 @@
+print("This is day 1")
+print("I'm going to become a python professional")
+
+print ("First line\nSecond line")
+
+print("Hello " + "Wasim")
+
+print ("String concatination is done with \"+\" sign")
+
+print ('String concatination is done with "+" sign')
